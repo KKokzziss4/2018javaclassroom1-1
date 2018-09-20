8744e324f9e2714d393ccc3a51b19a79bf05d699
KKokzziss4 <42628457+KKokzziss4@users.noreply.github.com>
2018-09-20 16:25:04 +0900

    Add files via upload

--- 자바기초실습/src/Test2.java
+++ 자바기초실습/src/Test2.java
@@ -1,15 +1,42 @@
-package 제06차시;
+package 제05차시;
 
 public class Test2 {
-	int a=1;
-	static int b=30;
+	int a=1, b=2, c=3, d=4;
 	
-	Test2() {
+	Test2(){
+		
+		int a = 4;
+		System.out.println("a:" + a);
+		
+		System.out.print("c:");
+		for(int b=1; b <=5; b++) {
+			int c = 5;
+			System.out.print(c + " ");
+			a= a+b;
+			
+		}
+		System.out.println("a:" + a +", b:" + b);
+		System.out.println("c:" + c);
+		
+		int b= 6;
+		System.out.println("b:" + b);
+		
+		if(c<4) {
+			int d=7;
+			System.out.println("d:"+ d);
+		}
+		System.out.println("d:"+ d);
+		
+		method1(8);
+		System.out.println("a:" + a + ",b:" + b +",c:" + c);
+		
 	}
 	
-	void print() {
-		System.out.println("a=" + a);
-		System.out.println("b=" + b);
+	void method1(int a) {
+		int b=9;
+		System.out.println("a:" + a + ",b:" + b +",c:" + c);
 	}
+	
 
 }
+
