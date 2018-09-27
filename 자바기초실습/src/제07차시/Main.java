e9efec52d931cca1a5ce512c035d14bbb2315ee8
KKokzziss4 <42628457+KKokzziss4@users.noreply.github.com>
2018-09-27 11:12:26 +0900

    한지수

---
+++ 자바기초실습/src/제07차시/Main.java
@@ -0,0 +1,14 @@
+package 제07차시;
+
+public class Main {
+	public static void main(String[] args) {
+		Car myCar = new Car();
+
+		// myCar 객체의 필드(속성, 멤버변수)들을 지정하세요.
+		
+		// myCar 객체의 필드 값들을 모두 출력하세요.
+
+		// myCar 객체의 메소드를 호출하여 동작시키세요.
+
+	}
+}
